bc7578894f9b4a08429262e686e94471f6010a09
ChrisAnhThien <nathien2310@gmail.com>
2025-01-14 23:36:48 -0500

    changes in nav

--- components/header.jsx
+++ components/header.jsx
@@ -1,19 +1,32 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
-import Nav from "./Nav"
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between items-center">
             {/*logo */}
             <Link href="/">
                 <h1 className="text-4xl font-semibold">
                     Chris<span className="text-accent">..</span>
                 </h1>
             </Link>
-            {/* Desktop nav */}
+
+            {/* Desktop nav and Hire me button */}
+            <div className="hidden xl:flex items-center gap-8">
             <Nav/>
+            <Link href="/contact">
+              <Button>
+                Hire me
+              </Button>
+            </Link>
+
+            </div>
+
+            {/* Mobile Nav */}
+            <div className="xl:hidden">Mobile Nav</div>
+            
         </div>
     </header>
   )
