5f3a795ea183294c5b6479c8283faecb1f71c09a
ChrisAnhThien <nathien2310@gmail.com>
2025-02-03 09:37:54 -0500

    add features navigation

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,12 @@ import {FiDownload} from "react-icons/fi"
 
 const Home = () => {
   return <section className="h-full">
-    <div className="container mx-auto">homepage</div>
+    <div className="container mx-auto h-full">
+      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div>Text</div>
+        <div>Photo</div>
+      </div>
+    </div>
 
   </section>
 }
--- components/header.jsx
+++ components/header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             {/*logo */}
             <Link href="/">
                 <h1 className="text-4xl font-semibold">
-                    Chris Thien<span className="text-accent">..</span>
+                    Chris<span className="text-accent">..</span>
                 </h1>
             </Link>
 
